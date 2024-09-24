d39e3968d30d73b2186a34a843083f39b17d5916
therajusah <workwithrajukumar@gmail.com>
2024-09-24 16:47:41 +0530

    Moved signupInput from Backend/src/zod.ts to common/src/index and removed Backend/src/zod.ts file

--- Backend/src/routes/user.ts
+++ Backend/src/routes/user.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { sign } from "hono/jwt";
-import { signupInput } from "../zod";
+import { signupInput } from "../../../common/src/index";
 
 export const userRouter = new Hono<{
   Bindings: {
